23949ad140aa2a30b07bd53d23456ff493e30aa1
Dmytro Shynkevych <dm.shynk@gmail.com>
2018-06-02 13:48:06 -0400

    Expanded interface and added an explanation to README

--- cosa-analog-pin-reader/include/AnalogPinSensor.h
+++ cosa-analog-pin-reader/include/AnalogPinSensor.h
@@ -13,9 +13,9 @@ namespace wlp {
             : analog_pin(analog_pin) {}
         void begin();
         void sleep();
-        double read_value();
     protected:
-        double read_voltage();
+        double read_voltage_raw();
+        double read_voltage_scaled(double scale, double bias = 0.0);
     private:
         AnalogPin analog_pin;
     };
--- cosa-analog-pin-reader/src/AnalogPinSensor.cpp
+++ cosa-analog-pin-reader/src/AnalogPinSensor.cpp
@@ -2,12 +2,21 @@
 
 using namespace wlp;
 
-double AnalogPinReader::read_voltage() {
+/* Read raw voltage on pin (range 0...5 V) */
+double AnalogPinReader::read_voltage_raw() {
     return analog_pin.sample() * VOLTAGE_SCALE;
 }
 
+/* Shift voltage by given bias, then scale.
+** For example, bias = 2.5 and scale = 100 will output 0
+** when raw voltage is 2.5 V and 100 when raw voltage is 3.5 V.
+*/
+double AnalogPinReader::read_voltage_scaled(double scale, double bias) {
+    return (read_voltage_raw() - bias) * scale;
+}
+
 double AnalogPinReader::read_value() {
-    return read_voltage();
+    return read_voltage_raw();
 }
 
 void AnalogPinReader::begin() {
