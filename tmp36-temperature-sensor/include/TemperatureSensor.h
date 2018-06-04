48b2e9ff6ad11ddaf5865a2164b32fe27a35c147
Jeff Niu <jeffniu22@gmail.com>
2018-06-04 11:00:27 -0400

    Removing virtual from sensor headers (#6)
    * Update TiltSensor.h
    
    * Update PressureSensor.h
    
    * Update TemperatureSensor.h

--- tmp36-temperature-sensor/include/TemperatureSensor.h
+++ tmp36-temperature-sensor/include/TemperatureSensor.h
@@ -9,7 +9,7 @@ namespace wlp {
         public:
             explicit TemperatureSensor(Board::AnalogPin analog_pin)
             : AnalogPinReader(analog_pin) {};
-            virtual double read_value() override;
+            double read_value() override;
         };
     }
 }
